9e45166610003d818deeba5471173e749b272dab
MJacobs92 <marco@marco>
2017-04-25 01:21:48 -0400

    added books.js stub code to handle ajax request

---
+++ books.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function initialLoad() {
+	$('listBooks').onclick = addButtonClicked;
+}
+
+function submitRequest() {
+	 new Ajax.Request('/booklist.php', {
+     	method: 'get',
+        onSuccess: handleRequestSuccess,
+        onFailure:  handleRequestFailure,
+        onException:  handleRequestFailure
+     });
+}
+
+function handleRequestSuccess(ajax) {
+	alert(ajax.responseXML);
+}
+
+function handleRequestFailure(ajax, exception) {
+	alert("Error making Ajax request: " + "\n\nServer status:\n" + ajax.status + " " + ajax.statusText +
+		"\n\nServer response text:\n" + ajax.responseText);
+	if(exception) {
+		throw exception;
+	}
+
+}
