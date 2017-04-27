53731c5c36855c80ba8ac296c9c3624810f7f990
MJacobs92 <marco@marco>
2017-04-27 10:32:32 -0400

    added index.php which is a duplicate of books.php. Just for hosting reasons

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AJAX Book Search</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js" type="text/javascript"></script>
+	<script src="books.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="categories">
+		
+	</div>
+	<div id="bookCatDiv">
+	</div>
+	<div id="booksDiv">
+	</div>
+</body>
+</html>
